3c9eb0d89b6771727903efba4939cd90fcbdfc9a
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2025-08-20 11:06:32 +0100

    Final keyword

--- src/beyondBasics/PublicStaticVoid/Main.java
+++ src/beyondBasics/PublicStaticVoid/Main.java
@@ -1,7 +1,10 @@
 package beyondBasics.PublicStaticVoid;
+import java.awt.*;
 import java.util.Arrays;
 
 public class Main {
+
+    public static final String NAME = "Pedro";
     public static void main(String[] args) {
         System.out.println("Program was given");
         if(args.length > 0) {
@@ -10,6 +13,42 @@ public class Main {
         } else {
             System.out.println("No arguments were given");
         }
-    }
 
+        // BREAK
+        var numbers = new int[]{1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            if (number == 3) {
+                break;
+            }
+            System.out.println(number);
+        }
+
+        // CONTINUE
+        var names = new String[]{
+                "Leopoldo",
+                "Andrew",
+                "Luciana",
+                "Allan",
+                "Pedro",
+                "Sara",
+                "Felipe"
+        };
+
+        for (String name : names) {
+            if (name.startsWith("A")) {
+                continue;
+            }
+            System.out.println(name);
+        }
+        System.out.println("End of program");
+
+        // FINAL KEYWORD
+        final var name = "Pedro";
+//        name = "Pedro";
+        System.out.println(NAME);
+        final Point p = new Point();
+        p.y = 10;
+        p.y = 90;
+        System.out.println(Math.PI);
+    }
 }
